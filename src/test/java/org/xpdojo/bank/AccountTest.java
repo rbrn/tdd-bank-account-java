e1301804a711c7bfd6ad3f91a45a39b9980e8e17
rbrn <costinaldea@yahoo.co.uk>
2021-01-28 17:13:04 +0200

    initial build

--- src/test/java/org/xpdojo/bank/AccountTest.java
+++ src/test/java/org/xpdojo/bank/AccountTest.java
@@ -12,4 +12,45 @@ public class AccountTest {
     public void depositAnAmountToIncreaseTheBalance() {
         assertThat("your first test").isBlank();
     }
+
+    @Test
+    public void accountIs0_Ok() {
+        Account account = new Account();
+        assertThat(account.balance()).isEqualTo(0);
+    }
+
+    @Test
+    public void deposit_money_To_accountsSuccessful() {
+        Account account = new Account();
+        account.deposit(100);
+        assertThat(account.balance()).isEqualTo(100);
+    }
+
+    @Test
+    public void depositMultipleAmountsSuccessful() {
+        Account account = new Account();
+        account.deposit(100);
+        account.deposit(200);
+        assertThat(account.balance()).isEqualTo(300);
+    }
+
+    //I can Withdraw money from accounts
+    @Test
+    public void withdraw_money_from_accountsSuccessful() {
+    }
+    //I can Transfer amounts between accounts (if I have the funds)
+    @Test
+    public void transfer_money_between_accountsSuccessful() {
+    }
+
+
+    //I can print out an Account balance slip (date, time, balance)
+    @Test
+    public void printStatementAccount_Successful() {
+    }
+
+    @Test
+    public void printStatementAccountWithFilters_Successful() {
+
+    }
 }
